b448e57fe3a9d782ee77536559b5968bb9bd8196
yulong <aquila198889@gmail.com>
2017-11-04 23:37:10 +0800

    添加了Clog

--- app/src/main/java/com/aquila/customWidget/MainActivity.java
+++ app/src/main/java/com/aquila/customWidget/MainActivity.java
@@ -3,11 +3,19 @@ package com.aquila.customWidget;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.aquila.customWidget.log.CLog;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        CLog.debug();
+        CLog.v();
+        CLog.d();
+        CLog.i();
+        CLog.e();
+        CLog.w();
     }
 }
---
+++ app/src/main/java/com/aquila/customWidget/log/CLog.java
@@ -0,0 +1,410 @@
+package com.aquila.customWidget.log;
+
+
+import android.text.TextUtils;
+import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.PrintWriter;
+import java.io.StringReader;
+import java.io.StringWriter;
+
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Source;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.stream.StreamResult;
+import javax.xml.transform.stream.StreamSource;
+
+public final class CLog {
+
+    private static final String LINE_SEPARATOR = System.getProperty("line.separator");
+    private static final String NULL_TIPS = "Log with null object";
+
+    private static final String DEFAULT_MESSAGE = "";
+    private static final String PARAM = "Param";
+    private static final String NULL = "null";
+    private static final String TAG_DEFAULT = "CLog_";
+    private static final String SUFFIX = ".java";
+
+    private static final int JSON_INDENT = 4;
+
+    private static final int V = 0x1;
+    private static final int D = 0x2;
+    private static final int I = 0x3;
+    private static final int W = 0x4;
+    private static final int E = 0x5;
+    private static final int A = 0x6;
+
+    private static final int JSON = 0x7;
+    private static final int XML = 0x8;
+
+    private static final int STACK_TRACE_INDEX_5 = 5;
+    private static final int STACK_TRACE_INDEX_4 = 4;
+    private static final int MAX_LENGTH = 4000;
+
+    private static boolean IS_SHOW_LOG = true;
+
+    public static void init(boolean isShowLog) {
+        IS_SHOW_LOG = isShowLog;
+    }
+
+
+    public static void v() {
+        printLog(V, null, DEFAULT_MESSAGE);
+    }
+
+    public static void v(Object msg) {
+        printLog(V, null, msg);
+    }
+
+    public static void v(String tag, Object... objects) {
+        printLog(V, tag, objects);
+    }
+
+    public static void d() {
+        printLog(D, null, DEFAULT_MESSAGE);
+    }
+
+    public static void d(Object msg) {
+        printLog(D, null, msg);
+    }
+
+    public static void d(String tag, Object... objects) {
+        printLog(D, tag, objects);
+    }
+
+    public static void i() {
+        printLog(I, null, DEFAULT_MESSAGE);
+    }
+
+    public static void i(Object msg) {
+        printLog(I, null, msg);
+    }
+
+    public static void i(String tag, Object... objects) {
+        printLog(I, tag, objects);
+    }
+
+    public static void w() {
+        printLog(W, null, DEFAULT_MESSAGE);
+    }
+
+    public static void w(Object msg) {
+        printLog(W, null, msg);
+    }
+
+    public static void w(String tag, Object... objects) {
+        printLog(W, tag, objects);
+    }
+
+    public static void e() {
+        printLog(E, null, DEFAULT_MESSAGE);
+    }
+
+    public static void e(Object msg) {
+        printLog(E, null, msg);
+    }
+
+    public static void e(String tag, Object... objects) {
+        printLog(E, tag, objects);
+    }
+
+    public static void a() {
+        printLog(A, null, DEFAULT_MESSAGE);
+    }
+
+    public static void a(Object msg) {
+        printLog(A, null, msg);
+    }
+
+    public static void a(String tag, Object... objects) {
+        printLog(A, tag, objects);
+    }
+
+    public static void json(String jsonFormat) {
+        printLog(JSON, null, jsonFormat);
+    }
+
+    public static void json(String tag, String jsonFormat) {
+        printLog(JSON, tag, jsonFormat);
+    }
+
+    public static void xml(String xml) {
+        printLog(XML, null, xml);
+    }
+
+    public static void xml(String tag, String xml) {
+        printLog(XML, tag, xml);
+    }
+
+
+    public static void debug() {
+        printDebug(null, DEFAULT_MESSAGE);
+    }
+
+    public static void debug(Object msg) {
+        printDebug(null, msg);
+    }
+
+    public static void debug(String tag, Object... objects) {
+        printDebug(tag, objects);
+    }
+
+    public static void trace() {
+        printStackTrace();
+    }
+
+    private static void printStackTrace() {
+
+        if (!IS_SHOW_LOG) {
+            return;
+        }
+
+        Throwable tr = new Throwable();
+        StringWriter sw = new StringWriter();
+        PrintWriter pw = new PrintWriter(sw);
+        tr.printStackTrace(pw);
+        pw.flush();
+        String message = sw.toString();
+
+        String traceString[] = message.split("\\n\\t");
+        StringBuilder sb = new StringBuilder();
+        sb.append("\n");
+        for (String trace : traceString) {
+            if (trace.contains("at com.socks.library.CLog")) {
+                continue;
+            }
+            sb.append(trace).append("\n");
+        }
+        String[] contents = wrapperContent(STACK_TRACE_INDEX_4, null, sb.toString());
+        String tag = contents[0];
+        String msg = contents[1];
+        String headString = contents[2];
+        printDefault(D, tag, headString + msg);
+    }
+
+    private static void printLog(int type, String tagStr, Object... objects) {
+
+        if (!IS_SHOW_LOG) {
+            return;
+        }
+
+        String[] contents = wrapperContent(STACK_TRACE_INDEX_5, tagStr, objects);
+        String tag = contents[0];
+        String msg = contents[1];
+        String headString = contents[2];
+
+        switch (type) {
+            case V:
+            case D:
+            case I:
+            case W:
+            case E:
+            case A:
+                printDefault(type, tag, headString + msg);
+                break;
+            case JSON:
+                printJson(tag, msg, headString);
+                break;
+            case XML:
+                printXml(tag, msg, headString);
+                break;
+        }
+
+    }
+
+    private static void printDebug(String tagStr, Object... objects) {
+        String[] contents = wrapperContent(STACK_TRACE_INDEX_5, tagStr, objects);
+        String tag = contents[0];
+        String msg = contents[1];
+        String headString = contents[2];
+        printDefault(D, tag, headString + msg);
+    }
+
+
+
+    private static String[] wrapperContent(int stackTraceIndex, String tagStr, Object... objects) {
+
+        StackTraceElement[] stackTrace = Thread.currentThread().getStackTrace();
+        StackTraceElement targetElement = stackTrace[stackTraceIndex];
+        String className = targetElement.getClassName();
+        String[] classNameInfo = className.split("\\.");
+        if (classNameInfo.length > 0) {
+            className = classNameInfo[classNameInfo.length - 1] + SUFFIX;
+        }
+
+        if (className.contains("$")) {
+            className = className.split("\\$")[0] + SUFFIX;
+        }
+
+        String methodName = targetElement.getMethodName();
+        int lineNumber = targetElement.getLineNumber();
+
+        if (lineNumber < 0) {
+            lineNumber = 0;
+        }
+
+        String tag = (tagStr == null ? TAG_DEFAULT + className : TAG_DEFAULT + tagStr);
+
+        String msg = (objects == null) ? NULL_TIPS : getObjectsString(objects);
+        String headString = "【(" + className + ":" + lineNumber + ")#" + methodName + "】 ";
+        return new String[]{tag, msg, headString};
+    }
+
+    private static String getObjectsString(Object... objects) {
+        if (objects.length > 1) {
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.append("\n");
+            for (int i = 0; i < objects.length; i++) {
+                Object object = objects[i];
+                if (object == null) {
+                    stringBuilder.append(PARAM).append("[").append(i).append("]").append(" = ").append(NULL).append("\n");
+                } else {
+                    stringBuilder.append(PARAM).append("[").append(i).append("]").append(" = ").append(object.toString()).append("\n");
+                }
+            }
+            return stringBuilder.toString();
+        } else {
+            Object object = objects[0];
+            return object == null ? NULL : object.toString();
+        }
+    }
+
+    private static void printDefault(int type, String tag, String msg) {
+        if (!IS_SHOW_LOG){
+            return;
+        }
+        int index = 0;
+        int length = msg.length();
+        int countOfSub = length / MAX_LENGTH;
+        if (countOfSub > 0) {
+            for (int i = 0; i < countOfSub; i++) {
+                String sub = msg.substring(index, index + MAX_LENGTH);
+                printSub(type, tag, sub);
+                index += MAX_LENGTH;
+            }
+            printSub(type, tag, msg.substring(index, length));
+        } else {
+            printSub(type, tag, msg);
+        }
+    }
+
+    private static void printSub(int type, String tag, String sub) {
+        switch (type) {
+            case V:
+                Log.v(tag, sub);
+                break;
+            case D:
+                Log.d(tag, sub);
+                break;
+            case I:
+                Log.i(tag, sub);
+                break;
+            case W:
+                Log.w(tag, sub);
+                break;
+            case E:
+                Log.e(tag, sub);
+                break;
+            case A:
+                Log.wtf(tag, sub);
+                break;
+        }
+    }
+
+
+    public static void printJson(String tag, String msg, String headString) {
+        if (!IS_SHOW_LOG){
+            return;
+        }
+
+        String message;
+        try {
+            if (msg.startsWith("{")) {
+                JSONObject jsonObject = new JSONObject(msg);
+                message = jsonObject.toString(JSON_INDENT);
+            } else if (msg.startsWith("[")) {
+                JSONArray jsonArray = new JSONArray(msg);
+                message = jsonArray.toString(JSON_INDENT);
+            } else {
+                message = msg;
+            }
+        } catch (JSONException e) {
+            message = msg;
+        }
+        StringBuffer sb = new StringBuffer();
+        sb.append(headString);
+        sb.append("\n");
+        sb.append(getLine(true));
+        String[] lines = message.split(LINE_SEPARATOR);
+        for (String line : lines) {
+            sb.append("\n");
+            sb.append("║" + line);
+        }
+        sb.append("\n");
+        sb.append(getLine(false));
+        Log.d(tag, sb.toString());
+
+    }
+
+    public static void printXml(String tag, String xml, String headString) {
+        if (!IS_SHOW_LOG){
+            return;
+        }
+
+        if (xml != null) {
+            xml = formatXML(xml);
+            xml = headString + "\n" + xml;
+        } else {
+            xml = headString + NULL_TIPS;
+        }
+        StringBuilder sb = new StringBuilder();
+
+        sb.append(headString)
+            .append("\n")
+            .append(getLine(true));
+
+        String[] lines = xml.split(LINE_SEPARATOR);
+        for (String line : lines) {
+            if (!isEmpty(line)) {
+                sb.append("\n")
+                    .append("║ " + line);
+            }
+        }
+        sb.append(getLine(false));
+        Log.d(tag, sb.toString());
+    }
+
+    private static String formatXML(String inputXML) {
+        try {
+            Source xmlInput = new StreamSource(new StringReader(inputXML));
+            StreamResult xmlOutput = new StreamResult(new StringWriter());
+            Transformer transformer = TransformerFactory.newInstance().newTransformer();
+            transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+            transformer.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "2");
+            transformer.transform(xmlInput, xmlOutput);
+            return xmlOutput.getWriter().toString().replaceFirst(">", ">\n");
+        } catch (Exception e) {
+            e.printStackTrace();
+            return inputXML;
+        }
+    }
+
+    private static boolean isEmpty(String line) {
+        return TextUtils.isEmpty(line) || line.equals("\n") || line.equals("\t") || TextUtils.isEmpty(line.trim());
+    }
+
+    private static String getLine(boolean isTop){
+        if (isTop){
+            return "╔═══════════════════════════════════════════════════════════════════════════════════════";
+        } else {
+            return "╚═══════════════════════════════════════════════════════════════════════════════════════";
+
+        }
+
+    }
+}
